de6a68210e84b52384cf1ba80f0007cc32d9621d
Rene Rosa <realpasro12@gmail.com>
2025-10-18 14:44:28 -0500

    Fix vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "frontend",
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    port: 5173,
+  },
 });
